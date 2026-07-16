2db1039fabe37c88c3200b97930f5ee42a7b3d65
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-07-16 20:01:22 +0200

    data flow

--- lua/docker/init.lua
+++ lua/docker/init.lua
@@ -9,6 +9,11 @@ function Docker.setup(user_config)
     Config.setup(user_config)
 
     vim.api.nvim_create_user_command("Docker", function()
+        -- Future flow:
+        -- 1. Fetch all Docker containers.
+        -- 2. Open the UI with the current container list.
+        -- 3. Start listening for Docker events.
+        -- 4. Update the UI when the container state changes.
         UI.open("docker")
     end, {
         desc = "Open Docker containers",
@@ -16,6 +21,12 @@ function Docker.setup(user_config)
     })
 
     vim.api.nvim_create_user_command("DockerCompose", function()
+        -- Future flow:
+        -- 1. Detect the current Docker Compose project.
+        -- 2. Fetch containers belonging to that project.
+        -- 3. Open the UI with the project container list.
+        -- 4. Start listening for project-related Docker events.
+        -- 5. Update the UI when the container state changes.
         UI.open("compose")
     end, {
         desc = "Open Docker Compose project containers",
